536a2f99ff6b0b110d4ecdf0d8ee813b7f10af29
LYH513 <peperony367@gmail.com>
2024-11-27 11:43:55 +0900

    test 파일 및 코드 추가

---
+++ src/Components/Home_Components/Test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Test(){
+
+  return(
+    <div>
+      테스트
+    </div>
+    
+  )
+}
+
+export default Test;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Test from "../Components/Home_Components/Test";
 
 //Home_Components의 컴포넌트들을 여기 불러오면 됨. 화면에 띄울 페이지
 function HomePage(){
@@ -8,6 +9,10 @@ function HomePage(){
     <Div>
       <ContentsDiv>
         홈 페이지
+        <Test/>
+        <Hi>
+          하이
+        </Hi>
       </ContentsDiv>
     </Div>
   );
@@ -26,4 +31,21 @@ const ContentsDiv = styled.div`
   display: flex;
   flex-direction: column;
   width: 80%;
+`
+
+const Hi = styled.div`
+  display: flex;
+  width: 32.2px;
+  height: 17px;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+
+  color: #000;
+  text-align: center;
+  font-family: Arial;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px; /* 125% */
 `
